f954306a6a7fc0ea198d22b50c15908aacd2ecf6
SnowDogge <128687888+SnowDogge@users.noreply.github.com>
2024-02-16 18:00:31 +0900

    240216

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -21,44 +21,33 @@ $(document).ready(function(){
         console.log(device_status)
     }
 
-    $(window).scroll(function(){
-        scroll_chk();
-    });
-    $(window).resize(function(){
-        webresize()
-    })
-    $('.header').on('mouseenter focusin', function() {
-        $('header').addClass('menu_over')
-    })
-    $('.header').on('mouseleave', function() {
-        $('header').removeClass('menu_over')
-    })
+    const swiper = new Swiper('.visual .swiper', {
 
-    //visual 팝업을 작동시키는 라이브러리
-    const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        effect: "fade",
 
-        effect: "fade", /* fade 효과 */
-
-        autoplay: {  /* 팝업 자동 실행 */
+        autoplay: {
             delay: 5000,
             disableOnInteraction: true,
         },
 
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        loop: true,
 
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.visual .pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            //type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        pagination: { 
+            el: '.visual .pagination',
+            clickable: true, 
         },
 
-        navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual .next',  /* 다음 버튼의 클래스명 */
+        navigation: {
+            nextEl: '.visual .next',
             prevEl: '.visual .prev',  
         },
 
     });
-      /* 일시정지 기능 */
+
+    $('.interview .list ul li').on('mouseenter', function(){
+        $('.interview .list ul li').removeClass('active');
+        $(this).addClass('active');
+    });
 
     $('.visual .swiper .stop').on('click', function(){
         $('.visual .swiper .play').show();
@@ -70,4 +59,20 @@ $(document).ready(function(){
         $('.visual .swiper .play').hide();
         swiper.autoplay.start();
     })
+
+    $(window).scroll(function(){
+        scroll_chk();
+    });
 })
+
+
+
+/* $(window).resize(function(){
+        webresize()
+    })
+    $('.header').on('mouseenter focusin', function() {
+        $('header').addClass('menu_over')
+    })
+    $('.header').on('mouseleave', function() {
+        $('header').removeClass('menu_over')
+    }) */
