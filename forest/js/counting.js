6769a3b5c8dd323af0ab59d34996128f3582923a
SnowDogge <128687888+SnowDogge@users.noreply.github.com>
2024-03-21 16:19:50 +0900

    240321

---
+++ forest/js/counting.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    function counting (){
+        $('.counter').each(function() { // .count-num에 각각 적용
+            var $this = $(this),
+            countTo = $this.attr('data-end');
+                
+            $({countNum: $this.text()}).animate({
+                countNum: countTo 
+                },{
+                duration: 3000,
+                easing:'linear',
+                step: function() {
+                $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                $this.text(this.countNum);
+                }
+            });  
+        });
+    }
+})
