a75b671b9b5705281a3864b552b8e57095c1a6c9
SnowDogge <128687888+SnowDogge@users.noreply.github.com>
2024-04-01 17:59:40 +0900

    20240401

--- 01.증평보건소/js/common.js
+++ 01.증평보건소/js/common.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+    var scroll_prev
 
+    $('footer .footer_top .f_link .open').on('click', function(){
+        $(".f_link").addClass('open');
+    })
+    $('footer .footer_top .f_link .close').on('click', function(){
+        $(".f_link").removeClass('open');
+    })
+
+    function scroll_chk(){
+        var scrolled = $(window).scrollTop();
+        console.log(scroll_prev);
+        console.log(scrolled);
+        if(scroll_prev < scrolled) {
+            $('header').attr('style','transform: translate(0, -50px)');
+        } else {
+            $('header').attr('style','transform: translate(0, 0)');
+        }
+        scroll_prev = scrolled;
+    }
+    scroll_chk();
+    $(window).scroll(function(){
+        scroll_chk();
+    })
 })
