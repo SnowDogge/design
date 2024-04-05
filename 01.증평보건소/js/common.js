3c0a99cf71c66f9274949740a7202a7cea0e391c
SnowDogge <128687888+SnowDogge@users.noreply.github.com>
2024-04-05 10:42:23 +0900

    20240405

--- 01.증평보건소/js/common.js
+++ 01.증평보건소/js/common.js
@@ -9,47 +9,60 @@ $(document).ready(function(){
         $(".f_link").removeClass('open');
     })
 
-    if(device_status = "pc"){
         $('header .gnb .depth1 > li').on('mouseenter focusin', function(){
-            $('header .gnb .depth1 > li').removeClass('on');
-            $(this).addClass('on');
-            $('header').addClass('menu_over');
+            if(device_status == 'pc') {
+                $('header .gnb .depth1 > li').removeClass('on');
+                $(this).addClass('on');
+                $('header').addClass('menu_over');
+            }
+            
         })
         $('header').on('mouseleave', function(){
-            $('header .gnb .depth1 > li').removeClass('on');
-            $('header').removeClass('menu_over');
-        })
-        $('header .gnb .depth1 > li:last-child > .depth2 > li:last-child > a').on('focusout', function(){
-            $('header .gnb .depth1 > li').removeClass('on');
-            $('header').removeClass('menu_over');
-        })
-        $('.header .gnb .gnb_open').on('click', function(){
-            $('.header').addClass('menu_open');
-            $('.container .bright').css({display : "block"});
-            $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
-        })
-        $('.header .gnb .gnb_close').on('click', function(){
-            $('.header').removeClass('menu_open');
-            $('.container .bright').css({display : "none"});
-            $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
-        })
-        $('header .gnb ul.depth1 > li > a').on("click", function(e){
-            e.preventDefault();
-            if($(this).parent().hasClass("on") === true) {
+            if(device_status == 'pc') {
                 $('header .gnb .depth1 > li').removeClass('on');
-            } else {
-                $('header .gnb .depth1 > li').removeClass('on');
-                $(this).parent().addClass('on');
+                $('header').removeClass('menu_over');
             }
         })
-    }
+        $('header .gnb .depth1 > li:last-child > .depth2 > li:last-child > a').on('focusout', function(){
+            if(device_status == 'pc') {
+                $('header .gnb .depth1 > li').removeClass('on');
+                $('header').removeClass('menu_over');
+            }
+        })
+        $('.header .gnb .gnb_open').on('click', function(){
+            if(device_status == 'mobile') {
+                $('.header').addClass('menu_open');
+                $('.container .bright').css({display : "block"});
+                $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+            }
+        })
+        $('.header .gnb .gnb_close').on('click', function(){
+            if(device_status == 'mobile') {
+                $('.header').removeClass('menu_open');
+                $('.container .bright').css({display : "none"});
+                $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
+            }
+        })
+        $('header .gnb ul.depth1 > li > a').on("click", function(e){
+            if(device_status == 'mobile') {
+                e.preventDefault();
+                if($(this).parent().hasClass("on") == false) {
+                    $('header .gnb .depth1 > li').removeClass('on');
+                    $(this).parent().addClass('on');
+                    
+                } else {
+                    $('header .gnb .depth1 > li').removeClass('on');
+                }
+            }
+        })
 
     function device_chk(){
         var window_w = $(window).width();
         if(window_w > 1240){
-            device_status = 'pc'
+            device_status = 'pc';
+            $('.container .bright').css({display : "none"});
         }else{
-            device_status = 'mobile'
+            device_status = 'mobile';
         }
         console.log(device_status);
         console.log(window_w);
@@ -61,6 +74,7 @@ $(document).ready(function(){
         console.log(scrolled);
         if(device_status == 'pc'){
             if(scroll_prev < scrolled) {
+                $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
                 $('header').attr('style','transform: translate(0, -50px)');
                 $('header').removeClass('menu_over')
             } else {
